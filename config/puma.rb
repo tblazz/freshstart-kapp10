da05e05361194f51d9c82b149fa6a33bdd6dc9bc
aaaldo <edouard.brethes@gmail.com>
2016-04-21 15:41:13 +0100

    correction bug sur config puma

--- config/puma.rb
+++ config/puma.rb
@@ -5,12 +5,6 @@ threads ENV['MIN_PUMA_WORKERS_COUNT'],ENV['MAX_PUMA_WORKERS_COUNT']
 workers 1
 preload_app!
 
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-end
-
 on_restart do
   $redis.reconnect
 end
