0544ab5d624702badf209d7f3691d0821956610e
Edouard Brèthes <edouard.brethes@lapptelier.com>
2016-07-14 11:58:39 +0100

    correction nom de paramètre erroné (race_name_mail)

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -37,7 +37,7 @@ class ResultController < ApplicationController
   def create
     #si on a un fichier
     if params[:file]
-      if params[:race_name] && params[:race_date] && params[:sender_mail] && params[:race_date_mail]
+      if params[:race_name] && params[:race_date] && params[:sender_mail] && params[:race_name_mail]
         #on récupère le fichier uploadé
         file = params[:file].read
         filename = params[:file].original_filename
