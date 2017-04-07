65f6a2fb1b5a04bec2cc95d6dd317752d1a7c477
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-04-07 13:13:29 +0200

    affiche un message SMS d'exemple.

--- app/models/race.rb
+++ app/models/race.rb
@@ -5,6 +5,10 @@ class Race < ActiveRecord::Base
   validates_attachment_content_type :raw_results, :content_type => ["text/plain", "text/csv"]
   validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\z/
 
+  before_save do |race|
+    self.sms_message = I18n.t('sms_message_template') if self.sms_message.blank?
+  end
+
   def runners_count
     @runners_count ||= results.count
   end
@@ -20,6 +24,7 @@ class Race < ActiveRecord::Base
   def emails_count
     @emails_count ||= results.pluck(:mail).select{|mail| mail =~ MAIL_REGEX}.length
   end
+
   def phones_count
     @phones_count ||= results.pluck(:phone).select{|phone| phone =~ PHONE_REGEX}.length
   end
--- app/models/result.rb
+++ app/models/result.rb
@@ -4,4 +4,16 @@ class Result < ActiveRecord::Base
   def rank_total
     rank == 1 ? I18n.t('first_suffix') : rank == 2 ? I18n.t('second_suffix') : I18n.t('third_suffix')
   end
+
+  def sent_message
+    race.sms_message % {
+      name: name,
+      time: time,
+      url: "image_path",
+      race_name_mail: race.email_name ,
+      rank: rank,
+      race_detail: race_detail,
+      results_url: race.results_url
+    }
+  end
 end
