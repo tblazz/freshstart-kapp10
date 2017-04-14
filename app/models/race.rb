4259d0b5ce01eb7a769d84746538f1e15c876cc7
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-04-14 15:03:25 +0200

    ajoute de nouveaux formats autorisés pour les fichiers de résultat

--- app/models/race.rb
+++ app/models/race.rb
@@ -27,7 +27,7 @@ class Race < ActiveRecord::Base
   has_attached_file :raw_results
   has_attached_file :background_image, styles: { medium: "300x300", standard: "1024x1024" }
   has_many :results, dependent: :destroy
-  validates_attachment_content_type :raw_results, :content_type => ["text/plain", "text/csv"]
+  validates_attachment_content_type :raw_results, :content_type => ["text/plain", "text/csv", "application/vnd.ms-excel", "text/comma-separated-values"]
   validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\z/
 
 
