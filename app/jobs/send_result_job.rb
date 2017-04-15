a0304e91342b967963b85ca4c7fbbe237120928f
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-04-15 12:23:22 +0200

    nouveau mailer pour les résultats

--- app/jobs/send_result_job.rb
+++ app/jobs/send_result_job.rb
@@ -3,27 +3,11 @@ class SendResultJob < ActiveJob::Base
 
   def perform(result)
     @result = Result.find(result)
-    @diploma = GenerateDiploma.new(@result).image_kit(IMAGE_HEIGHT,IMAGE_WIDTH)
-    image = SaveImageToS3.new(@result.race.name, @result.race.date, @result.bib, @diploma.to_img(:jpg)).call
-    short_image_path = Bitly.client.shorten(image[:image_path], history: 1).jmp_url
 
     # Envoi du mail
-    ResultMailer.mail_result(
-      @result.first_name,
-      @result.time,
-      @result.race.email_sender,
-      @result.race.name,
-      @result.race.email_name,
-      @result.race.hashtag,
-      @result.mail,
-      @result.rank_total,
-      @result.race.results_url,
-      @result.race.sms_message,
-      image[:image_file_name],
-      image[:image_path],
-      short_image_path).deliver_later if @result.mail =~ MAIL_REGEX
+    ResultMailer.mail_result(@result).deliver_later if @result.mail =~ MAIL_REGEX
 
     # Envoi du SMS
-    @result.sms.send(short_image_path)
+    # @result.sms.send
   end
 end
--- app/mailers/result_mailer.rb
+++ app/mailers/result_mailer.rb
@@ -1,28 +1,14 @@
 class ResultMailer < ApplicationMailer
 
   #Envoi un mail au participant
-  def mail_result(name, time, sender_mail, race_name, race_name_mail, hash_tag, mail_address, rank, results_url, sms_message, image_file_name, image_path, short_image_path)
-    if image_path && mail_address
-      subject = I18n.t('mail_subject', race_name_mail: race_name_mail)
-      @name = name
-      @time = time
-      @race_name = race_name
-      @race_name_mail = race_name_mail
-      @hash_tag = hash_tag
-      @url = short_image_path
-      @sms_message = sms_message % { name: name, time: time, url: short_image_path, race_name_mail: race_name_mail,rank: rank, results_url: results_url}
-      # @path = short_image_path
-      response = HTTParty.get(image_path)
-      if response
-        temp_file = Tempfile.new(SecureRandom.hex(20), "/tmp", :encoding => 'ascii-8bit')
-        temp_file.write(response.body)
+  def mail_result(result)
+    Rails.logger.debug 'bite'
+    @result = Result.find(result)
+    Rails.logger.debug(@result)
 
-        attachments[image_file_name] = File.read(temp_file)
-
-        mail to: mail_address, from: I18n.t('mail_sender', sender_mail: sender_mail), subject: subject
-
-        temp_file.unlink
-      end
+    if @result.diploma_url && @result.mail
+      subject = I18n.t('mail_subject', race_name_mail: @result.race.email_name)
+      mail to: @result.mail, from: I18n.t('mail_sender', sender_mail: @result.race.email_sender), subject: subject
     end
   end
 
