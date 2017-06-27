7ca4f04d7615e7f9c583d6e9d75cd73920102b93
Paula Lantran <paula.lantran@gmail.com>
2017-06-27 15:25:21 +0200

    Ajout nouveau template matahi

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -6,11 +6,13 @@ CSV_SEPARATOR = ";"
 IMAGE_HEIGHT = {
   'template1' => 1024,
   'texte-ombre' => 668,
+	'template_matahi' => 666,
 	'template_base1000x666' => 666
 }
 IMAGE_WIDTH = {
   'template1' => 1024,
   'texte-ombre' => 1000,
+	'template_matahi' => 1000,
 	'template_base1000x666' => 1000
 }
 
