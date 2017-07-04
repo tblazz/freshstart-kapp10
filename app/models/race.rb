de5d7179fee0e3bd394fbc9078d002da5b7182a5
Paula Lantran <paula.lantran@gmail.com>
2017-07-04 13:19:46 +0200

    Resize le widget pour faire rentrer tous les elements

--- app/models/race.rb
+++ app/models/race.rb
@@ -80,7 +80,7 @@ class Race < ActiveRecord::Base
 
   def widget_gist
 %(
-	<iframe class='kapp10-embed' src="//s3-eu-west-1.amazonaws.com/results-widget.kapp10.com/#{widget_storage_name}" frameborder="0" scrolling="no" frameborder="0" allowfullscreen="" style="border: none; width: 1px; min-width: 100%; *width: 100%; height: 100%; min-height: 1000px;" scrolling="no"></iframe>)
+	<iframe class='kapp10-embed' src="//s3-eu-west-1.amazonaws.com/results-widget.kapp10.com/#{widget_storage_name}" frameborder="0" scrolling="no" frameborder="0" allowfullscreen="" style="border: none; width: 1px; min-width: 100%; *width: 100%; height: 100%; min-height: 1100px;" scrolling="no"></iframe>)
   end
 
 	def photos_widget_gist
