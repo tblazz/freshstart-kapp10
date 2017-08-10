6d197fe078d823e7d5ea6f6749e2fa62bb3d35ff
Paula Lantran <paula.lantran@gmail.com>
2017-08-10 09:24:34 +0200

    Fix recherche coureurs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,15 @@ module ApplicationHelper
     image_tag(name_at_1x, options.merge("data-at2x" => asset_path(name_at_2x)))
   end
 
-	def sortable(column, title = nil)
+	def sortable(column, title = nil, race)
     title ||= column.titleize
 		css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-		link_to title,
+		link_to title, race_path(race,
   		params.merge(
     	:sort => column,
     	:direction => direction,
-    	:page => nil).permit(:sort, :direction, :page, :search),
+    	:page => nil).permit(:sort, :direction, :page, :search)),
   		{:class => css_class}
 		end
 end
