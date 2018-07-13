4e020b21364a91db7d02e68da42622ccf1bfb0b9
jemmeli <jemmeli84@gmail.com>
2018-07-13 15:41:04 +0300

    style event bloc

--- app/jobs/generate_client_widget_job.rb
+++ app/jobs/generate_client_widget_job.rb
@@ -57,30 +57,22 @@ class GenerateClientWidgetJob < ActiveJob::Base
 
     line = "<div class='event col-xs-12 firstTd clearfix' data-name-search='#{ event.name }' data-department-search='#{ event.department }' data-type-search='#{event.races.map(&:race_type).reject(&:blank?).join(' ')}' data-month-search='#{get_month(event.editions.last.date)}'>"
       line += "<div class='pull-left'>"
-        line += "<h3>#{ event.name }</h3>"
-        line += "<ul>"
-          line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/lieu.png'>#{ event.department }</div></li>"
-          line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/distance.png'>#{ event.races.map(&:name).uniq.join(', ') }</div></li>"
-          line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/type.png'>#{ event.races.map(&:race_type).reject(&:blank?).join(', ') }</div></li>"
-        line += "</ul>"
-      line += "</div>" 
-
+      line += "<h3>#{ event.name }</h3>"
+      line += "<ul>"
+        line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/lieu.png'>#{ event.department }</div></li>"
+        line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/distance.png'>#{ event.races.map(&:name).uniq.join(', ') }</div></li>"
+        line += "<li><div class='logo-edition'><img src='https://evenementwidget.herokuapp.com/assets/images/type.png'>#{ event.races.map(&:race_type).reject(&:blank?).join(', ') }</div></li>"
+      line += "</ul>"
+      line += "</div>"
       line += "<div class='pull-right'>"
-        line += "<div class='theDate'>"
-
-          line += "<ul class='list-inline'>"
-            line += "<li>"
-              line += "<div class='myDate'>#{ event.editions.last.date.strftime('%d/%m/%Y') }</div>"
-              line += "<div class='results-count'>#{ "#{event.editions.last.results.count} finisseurs" if event.editions.last.results.count > 0 }</div>"
-            line += "</li>"
-            line += "<li>"
-              line =+ "<div class='results-button'><a class='btn btn-warning' onclick=\"displayResultsPage('#{ event.editions.last.widget_url }')\">Voir les résultats</a></div>"
-            line += "</li>"
-          line += "</ul>"
-
-        line += "</div>"
+      line += "<div class='theDate'>"
+        line += "<div class='myDate'><div class='pull-right'>#{ event.editions.last.date.strftime('%d/%m/%Y') }</div></div>"
+        line += "<div class='row'><div class='pull-right results-count'><div>#{ "#{event.editions.last.results.count} finisseurs" if event.editions.last.results.count > 0 }</div></div></div>"
+        line += "<div class='row'><div class='pull-right results-button'><a class='btn btn-warning' onclick=\"displayResultsPage('#{ event.editions.last.widget_url }')\">Voir les résultats</a></div></div>"
       line += "</div>"
     line += "</div>"
+    line += "</div>"
+    line
   end
 
   def get_month(date)
