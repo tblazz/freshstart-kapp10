4b31b07f854e89f4e33b6eeb0de9a28cfbbb9521
EmCousin <emmanuel_cousin@hotmail.fr>
2018-07-31 13:08:41 +0200

    fix typo var

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -2,5 +2,5 @@ S3DirectUpload.config do |c|
   c.access_key_id     = Rails.configuration.aws[:access_key_id]
   c.secret_access_key = Rails.configuration.aws[:secret_access_key]
   c.bucket            = Rails.configuration.aws[:bucket]
-  c.region            = ENV["AWS_S3_HOST_NAME"]
+  c.region            = ENV["AWS_S3_HOST_NAME_REGION"]
 end
