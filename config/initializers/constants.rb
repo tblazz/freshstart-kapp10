398f0069df22bc92ebf0f22ba82d355980db1430
EmCousin <emmanuel_cousin@hotmail.fr>
2018-08-04 13:25:41 +0200

    add template raid

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -8,14 +8,16 @@ IMAGE_HEIGHT = {
   'texte-ombre' => 668,
 	'template_matahi' => 666,
 	'template_base1000x666' => 666,
-	'template_traverseebaie' => 712
+	'template_traverseebaie' => 712,
+  'template_raid' => 666
 }
 IMAGE_WIDTH = {
   'template1' => 1024,
   'texte-ombre' => 1000,
 	'template_matahi' => 1000,
 	'template_base1000x666' => 1000,
-	'template_traverseebaie' => 1000
+	'template_traverseebaie' => 1000,
+  'template_raid' => 1000
 }
 
 #mail
