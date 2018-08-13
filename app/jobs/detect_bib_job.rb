d2c795d1545c9287f7f491e2c26f8b570570bc24
EmCousin <emmanuel_cousin@hotmail.fr>
2018-08-13 15:47:02 +0200

    fix bug in detect bib job

--- app/jobs/detect_bib_job.rb
+++ app/jobs/detect_bib_job.rb
@@ -4,23 +4,23 @@ class DetectBibJob < ActiveJob::Base
   def perform(photo_id)
     return unless text.present?
 
-    photo = Photo.find(photo_id)
-    results = photo.edition.results
+    @photo = Photo.find(photo_id)
+    results = @photo.edition.results
     words = text.words
     bib = words.find { |w| w.text.in? results.pluck(:bibs) }
     return unless bib.present?
 
     result = result.find_by(bib: bib)
-    photo.race = result.race
-    photo.bib = bib
-    photo.save!
+    @photo.race = result.race
+    @photo.bib = bib
+    @photo.save!
   end
 
   private
 
   def text
     vision = Google::Cloud::Vision.new
-    image = vision.image photo.image.url
+    image = vision.image @photo.image.url
     annotation = vision.annotate image, text: true
     annotation.text
   end
