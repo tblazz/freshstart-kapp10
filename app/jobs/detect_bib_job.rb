d00afde460af2efb5fac5dd50c7358ef737ff8e9
EmCousin <emmanuel_cousin@hotmail.fr>
2018-08-13 19:38:34 +0200

    no longer use private method in detect bib job

--- app/jobs/detect_bib_job.rb
+++ app/jobs/detect_bib_job.rb
@@ -2,26 +2,23 @@ class DetectBibJob < ActiveJob::Base
   queue_as :normal
 
   def perform(photo_id)
-    return unless text.present?
+    photo = Photo.find(photo_id)
 
-    @photo = Photo.find(photo_id)
-    results = @photo.edition.results
+    vision = ::Google::Cloud::Vision.new
+    image = vision.image photo.image.url
+    annotation = vision.annotate image, text: true
+
+    return unless annotation.text.present?
+
+    results = photo.edition.results
     words = text.words
     bib = words.find { |w| w.text.in? results.pluck(:bib) }
+
     return unless bib.present?
 
     result = result.find_by(bib: bib)
-    @photo.race = result.race
-    @photo.bib = bib
-    @photo.save!
-  end
-
-  private
-
-  def text
-    vision = ::Google::Cloud::Vision.new
-    image = vision.image @photo.image.url
-    annotation = vision.annotate image, text: true
-    annotation.text
+    photo.race = result.race
+    photo.bib = bib
+    photo.save!
   end
 end
