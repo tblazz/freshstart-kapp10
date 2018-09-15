a91889ba916921a157fe1d4f2482ad6b25da5e2c
EmCousin <emmanuel_cousin@hotmail.fr>
2018-09-15 16:12:35 +0200

    fix conflict

--- app/models/result.rb
+++ app/models/result.rb
@@ -44,13 +44,9 @@ class Result < ActiveRecord::Base
   belongs_to :edition
   belongs_to :race
   belongs_to :runner
-<<<<<<< HEAD
-
   has_attached_file :diploma, styles: { freemium: '1000x666' },
                               source_file_options: { freemium: '-density 72' }
   validates_attachment_content_type :diploma, content_type: /\Aimage\/.*\z/
-=======
->>>>>>> Mise en place de l'API
 
   def sent_message
     sms.message
