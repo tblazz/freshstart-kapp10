a03929151678c3130818b9b6f342e547b5c27392
Tim Grc <tim@kuartz.fr>
2019-01-31 11:56:29 +0000

    Update searches_controller.rb

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::SearchesController < API::V1::ApplicationController
   def show
     event_name = params["search-input"]
-    @events = Event.where("name ILIKE ?", "%#{event_name}%" ).first(8)
+    @events = Event.where("name ILIKE ?", "%#{event_name}%" ).limit(8)
 
     render json: @events
   end
