42027d2d60849d9ee7ffa8eae99afc964eac2276
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-02-12 16:05:51 +0100

    api v2 edition

--- app/controllers/api/v2/editions_controller.rb
+++ app/controllers/api/v2/editions_controller.rb
@@ -1,9 +1,15 @@
 class API::V2::EditionsController < API::V2::ApplicationController
   def index
-    if params[:with_lastest_results].true?
-      @editions = Edition.with_lastest_results(params[:limit] || 3)
+    query_params = params["query_params"] || {}
+    number_of_elements_by_page = query_params['number_of_elements_by_page'] || 24
+    limit                      = query_params['number_of_elements_by_page'] || 3
+    page_number                = query_params['page_number'] || 1
+    offset                     = (page_number - 1) * number_of_elements_by_page
+
+    if query_params['with_lastest_results']
+      @editions = Edition.with_lastest_results(limit)
     else
-      @editions = Edition.order(created_at: :desc)
+      @editions = Edition.order(created_at: :desc).offset(offset).limit(number_of_elements_by_page)
     end
 
     render json: @editions
