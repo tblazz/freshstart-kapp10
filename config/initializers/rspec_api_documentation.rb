66ede875d5888e0b9fe9b335b1297d3244d92448
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-02-12 14:35:23 +0100

    api v2 for kuartz work

---
+++ config/initializers/rspec_api_documentation.rb
@@ -0,0 +1,9 @@
+RspecApiDocumentation.configure do |config|
+  # Output folder
+  config.docs_dir = Rails.root.join("doc", "api")
+
+  # An array of output format(s).
+  # Possible values are :json, :html, :combined_text, :combined_json,
+  #   :json_iodocs, :textile, :markdown, :append_json
+  config.format = [:html]
+end
