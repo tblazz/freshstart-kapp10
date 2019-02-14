5ac931e98e26670de544f0d9bb9e5c4485cc52e8
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-02-14 13:03:06 +0100

    debug runner api

--- app/controllers/api/v2/featured_runners_controller.rb
+++ app/controllers/api/v2/featured_runners_controller.rb
@@ -1,6 +1,6 @@
 class API::V2::FeaturedRunnersController < API::V2::ApplicationController
   def index
     @runners = Runner.order(created_at: :desc).limit(4)
-    render json: @runners
+    render json: @runners.to_json
   end
 end
