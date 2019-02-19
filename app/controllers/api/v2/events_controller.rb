d577558046e60faaf0c337f9bb63a3ba57c95987
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-02-19 15:53:30 +0100

    add range to date

--- app/controllers/api/v2/events_controller.rb
+++ app/controllers/api/v2/events_controller.rb
@@ -5,10 +5,11 @@ class API::V2::EventsController < API::V2::ApplicationController
     query_params = params[:query_params]
 
     if query_params.present?
-      date = query_params[:event_date]
+      begin_date = query_params[:begin_date]
+      end_date   = query_params[:end_date]
       
-      if date && date != ''
-        @events = Event.joins(:editions).where("DATE(editions.date) = ?", date)
+      if begin_date && begin_date != ''
+        @events = Event.joins(:editions).where("DATE(editions.date) >= ? AND DATE(editions.date) <= ?", begin_date, end_date)
       end
     end
 
