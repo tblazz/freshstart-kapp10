ca17d5bb9d2c695bbc587b1a003510ecb50ffa34
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-02-27 16:04:51 +0100

    order event by date

--- app/controllers/api/v2/events_controller.rb
+++ app/controllers/api/v2/events_controller.rb
@@ -1,6 +1,6 @@
 class API::V2::EventsController < API::V2::ApplicationController
   def index
-    @events = Event.order(:date)
+    @events = Event.joins(:editions).order('editions.date ASC')
 
     query_params = params[:query_params]
 
@@ -9,10 +9,9 @@ class API::V2::EventsController < API::V2::ApplicationController
       end_date   = query_params[:end_date]
       
       if begin_date && begin_date != ''
-        @events = Event.joins(:editions).where("DATE(editions.date) >= ? AND DATE(editions.date) <= ?", begin_date, end_date).order(:date)
+        @events = Event.joins(:editions).where("DATE(editions.date) >= ? AND DATE(editions.date) <= ?", begin_date, end_date).order('editions.date ASC')
       end
     end
-
 
     render json: @events.to_json
   end
