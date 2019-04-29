427eb047d47565475b01edadc587af531388b790
Timothee Garcia <grc.tmt@gmail.com>
2019-04-29 09:39:14 +0200

    Add domain kapp10.live to url shortener service

--- app/services/url_shortener_service.rb
+++ app/services/url_shortener_service.rb
@@ -2,12 +2,13 @@ class UrlShortenerService
   def initialize(destination)
     @destination   = destination
     @rebrandly_api = Rebrandly::Api.new
+    @domain        = @rebrandly_api.domains.first
   end
 
   def call
     return if @destination.empty?
 
-    rebrandly_link = @rebrandly_api.shorten(@destination)
+    rebrandly_link = @rebrandly_api.shorten(@destination, domain: @domain.to_h)
 
     rebrandly_link.short_url
   end
