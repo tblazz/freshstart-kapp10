b3322d71ef62c1b925e44b94c15d7f086e608e35
Timothee Garcia <grc.tmt@gmail.com>
2019-04-29 09:50:11 +0200

    Add ENV variable to specify rebrandly domain full name used in url shortener service

--- app/services/url_shortener_service.rb
+++ app/services/url_shortener_service.rb
@@ -2,14 +2,26 @@ class UrlShortenerService
   def initialize(destination)
     @destination   = destination
     @rebrandly_api = Rebrandly::Api.new
-    @domain        = @rebrandly_api.domains.first
+    @domain        = domain
   end
 
   def call
-    return if @destination.empty?
+    return unless !@destination.empty? && @domain
 
     rebrandly_link = @rebrandly_api.shorten(@destination, domain: @domain.to_h)
 
     rebrandly_link.short_url
+  end
+
+  private
+
+  def domain
+    selected_domains = @rebrandly_api.domains.select do |domain|
+      domain.full_name == ENV['REBRANDLY_DOMAIN_FULL_NAME'] && domain.active
+    end
+
+    return if selected_domains.empty?
+
+    selected_domains.first
   end
 end
