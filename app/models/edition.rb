2287325c76063731c74648398d95bb0f93cc7d88
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-07-08 10:02:23 +0200

    debug widget

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -131,7 +131,8 @@ class Edition < ApplicationRecord
       {
         url:   ENV['RAILS_ENV'] == 'development' ? photo.direct_image_url : photo.image.url,
         bib:   photo.bib,
-        rank:  (result && result.rank) ? result.rank : results.size + 1,
+        # rank:  (result && result.rank) ? result.rank : results.size + 1,
+        rank: rand(1..100),
         race:  result ? result.race_detail.parameterize : '',
         sex:   result ? result.sex.parameterize : '',
         categ: result ? result.categ.parameterize : '',
