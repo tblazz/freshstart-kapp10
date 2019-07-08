5fc466007382fdc78b1f20e31d3c766289a08196
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-07-08 10:29:58 +0200

    debug widget

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -129,14 +129,13 @@ class Edition < ApplicationRecord
       end
 
       {
-        # url:   ENV['RAILS_ENV'] == 'development' ? photo.direct_image_url : photo.image.url,
-        url:   "",
+        url:   ENV['RAILS_ENV'] == 'development' ? photo.direct_image_url : photo.image.url,
         bib:   photo.bib || "",
         rank:  (result && result.rank) ? result.rank : results.size + 1,
         race:  result ? result.race_detail.parameterize : '',
         sex:   result ? result.sex.parameterize : '',
         categ: result ? result.categ.parameterize : '',
-        name:  "",
+        name:  name.gsub(/'/, "\\\\'"), # fix problem with `'` in names 🤷
       }
     end
 
