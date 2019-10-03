6be23b9d7b919440d3b56d9c9b5a39055a68678c
Emmanuel Cohen-Laroque <ecohenlaroque@gmail.com>
2019-10-03 13:12:03 +0200

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   Rails.application.config.active_job.queue_adapter = :sidekiq
   # Do not dump schema after migrations.
-  # config.active_record.dump_schema_after_migration = false
+   config.active_record.dump_schema_after_migration = false
 end
