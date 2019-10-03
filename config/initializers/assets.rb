73a51be969c43037a0e222ff85b497642726f1a1
Emmanuel Cohen-Laroque <ecohenlaroque@gmail.com>
2019-10-03 12:51:22 +0200

    up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.1'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( diploma.css )
-Rails.application.config.assets.precompile += %w( diploma_form.sass )
+#Rails.application.config.assets.precompile += %w( diploma_form.sass )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
