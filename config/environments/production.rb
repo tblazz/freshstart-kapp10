7d1d54a8df3a66c5384f1c7b402bddf2369f143e
Emmanuel Cohen-Laroque <ecohenlaroque@gmail.com>
2019-10-04 10:00:57 +0200

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -26,8 +27,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
-
+  #config.public_file_server.enabled = true
+  config.serve_static_assets= true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
