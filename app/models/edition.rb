3af89db37d8edd9e80565c13d240215541306aed
najm <jemmeli84@gmail.com>
2020-09-20 19:53:09 +0100

    add binding pry

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -136,10 +136,10 @@ class Edition < ApplicationRecord
         url:   ENV['RAILS_ENV'] == 'development' ? photo.direct_image_url : photo.image.url,
         bib:   photo.bib || "",
         rank:  (result && result.rank) ? result.rank : results.size + 1,
-        race:  44,
-        sex:    66,
-        categ:  77,
-        name:  name.gsub(/'/, " "), # fix problem with `'` in names
+        race:  result ? result.race_detail.parameterize : '',
+        sex:   result ? result.sex.parameterize : '',
+        categ: result ? result.categ.parameterize : '',
+        name:  name.gsub(/'/, " ") # fix problem with `'` in names
       }
     end
 
